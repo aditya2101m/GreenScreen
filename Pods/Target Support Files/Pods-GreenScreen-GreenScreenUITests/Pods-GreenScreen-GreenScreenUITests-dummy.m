cac63c074decd98a957a8be50aae0037e20e859c
aditya2101m <apm6483@psu.edu>
2021-03-28 14:19:46 -0400

    workspace

---
+++ Pods/Target Support Files/Pods-GreenScreen-GreenScreenUITests/Pods-GreenScreen-GreenScreenUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GreenScreen_GreenScreenUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GreenScreen_GreenScreenUITests
+@end
---
+++ Pods/Target Support Files/Pods-GreenScreen/Pods-GreenScreen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GreenScreen : NSObject
+@end
+@implementation PodsDummy_Pods_GreenScreen
+@end
